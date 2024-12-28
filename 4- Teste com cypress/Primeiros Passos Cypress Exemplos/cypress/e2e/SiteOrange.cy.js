44e1748efd09202f56d6951dc7e9afcc8033789a
Felipo Blanc <felipo.blanc@csptech.com.br>
2024-12-28 13:53:51 -0300

    Importancia de aguardar elementos

--- 4- Teste com cypress/Primeiros Passos Cypress Exemplos/cypress/e2e/SiteOrange.cy.js
+++ 4- Teste com cypress/Primeiros Passos Cypress Exemplos/cypress/e2e/SiteOrange.cy.js
@@ -1,11 +1,12 @@
 describe('site Orange', () => {
 
 const comandos = {
-  inserirNome: "[name='username']",
-  inserirSenha: "[name='password']",
-  clicarEmSubmit: "[type='submit']",
-  alerta: "[role='alert']",
-  tituloTopo: '.oxd-topbar-header-breadcrumb-module'
+  inserirNome: "[name='username']", //Admin
+  inserirSenha: "[name='password']",//senha correta admin123
+  clicarEmSubmit: "[type='submit']",//botao de dar submit
+  alerta: "[role='alert']",//alerta de erro
+  tituloTopo: '.oxd-topbar-header-breadcrumb-module',// titulo da pagina
+  paginaDashboard:  '.orangehrm-dashboard-grid' //pagina dashboard inteira
   
   }  
 
@@ -28,5 +29,18 @@ const comandos = {
 
   
   })
+
+  it('Login com sucesso 2', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(comandos.inserirNome) .type('Admin') /// USUARIO CORRETO   
+    cy.get(comandos.inserirSenha) .type('admin123') /// SENHA CORRETA
+    cy.get(comandos.clicarEmSubmit).click() /// CLICAR EM LOGIN
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')///SISTEMA RETORNA PAGINA DO DASHBOARD
+    cy.get(comandos.paginaDashboard)/// SISTEMA RETORNA A PAGINA COMPLETA DO DASHBOARD
+    cy.get(comandos.tituloTopo).contains('Dashboard')/// SISTEMA RETORNA TITULO DA PAGINA = DASHBOARD
+    
+  })
+
+
 
 })

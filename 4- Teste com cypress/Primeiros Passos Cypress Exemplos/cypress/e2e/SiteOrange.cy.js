f7f582755299c93ec1842dea75096e18bdf065f6
Felipo Blanc <felipo.blanc@csptech.com.br>
2025-01-07 00:15:01 -0300

    06/01/2025

--- 4- Teste com cypress/Primeiros Passos Cypress Exemplos/cypress/e2e/SiteOrange.cy.js
+++ 4- Teste com cypress/Primeiros Passos Cypress Exemplos/cypress/e2e/SiteOrange.cy.js
@@ -2,161 +2,135 @@ import userData from '../fixtures/users/user-data.json'
 
 describe('site Orange', () => {
 
-  const elemento = {
+     const pageElements = {
+      // Login Page
+      inputUsername: "[name='username']", // Admin
+      inputPassword: "[name='password']", // Correct password: admin123
+      buttonSubmit: "[type='submit']", // Submit button
+      alert: "[role='alert']", // Error alert
     
-    //Pagina login
-    inserirNome: "[name='username']", //Admin
-    inserirSenha: "[name='password']",//senha correta admin123
-    clicarEmSubmit: "[type='submit']",//botao de dar submit
-    alerta: "[role='alert']",//alerta de erro
-
-    //Elementos pagina inicial do sistema = dashboard
-    tituloTopo: '.oxd-topbar-header-breadcrumb-module',// titulo da pagina
-    paginaDashboard:  '.orangehrm-dashboard-grid' ,//pagina dashboard inteira
-    myInfobotao:"[href='/web/index.php/pim/viewMyDetails']",
-
-    //Elementos Pagina MyInfo
-        //Primeira parte
-    primeiroNome:"[name='firstName']", 
-    sobrenome:"[name='middleName']",
-    ultimoNome:"[name='lastName']",
-
-      //Segunda parte 
-    dropdownNacionalidade:":nth-child(5) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input",
-    employeeId:':nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input',
-    otherId:':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input',
-
-      //Terceira parte
-    driveLicense:':nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input',
-    calenderlicenseExpiryDate:':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input',
-    inputCalenderlicenseExpiryDate:':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input',
-
-      //Quarta parte
-      nacionalityStatus: "[tabindex='0']", //nacionalidade e estado civil
-
-      //Quinta parte
-      dataNascimento:':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input',
+      // Dashboard Page Elements
+      headerTitle: '.oxd-topbar-header-breadcrumb-module', // Page title
+      dashboardPage: '.orangehrm-dashboard-grid', // Entire dashboard page
+      buttonMyInfo: "[href='/web/index.php/pim/viewMyDetails']",
+    
+      // My Info Page Elements
+      // First Part
+      inputFirstName: "[name='firstName']",
+      inputMiddleName: "[name='middleName']",
+      inputLastName: "[name='lastName']",
+    
+      // Second Part
+      dropdownNationality: ":nth-child(5) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input",
+      inputEmployeeId: ':nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input',
+      inputOtherId: ':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input',
+    
+      // Third Part
+      inputDriverLicense: ':nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input',
+      inputLicenseExpiryDate: ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input',
+    
+      // Fourth Part
+      nationalityStatus: "[tabindex='0']", // Nationality and marital status
+    
+      // Fifth Part
+      inputDateOfBirth: ':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input',
       femaleButton: ":nth-child(2) > :nth-child(2) > .oxd-radio-wrapper > label",
       maleButton: ':nth-child(1) > :nth-child(2) > .oxd-radio-wrapper > label',
-
-      //Sexta parte
-      bloodType: '.orangehrm-card-container > .oxd-form > .oxd-form-row > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input',
-      testField:'.orangehrm-card-container > .oxd-form > .oxd-form-row > .oxd-grid-3 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input',
     
-     //Elementos de Save
-    saveButton:"[type='submit']",
-    toastTitleSuccess:".oxd-text--toast-title",
-    toastMensageSuccess:".oxd-text--toast-message"
-    }  
-
-
-    // Login com sucesso padrão com o Const e o objeto userData em outro arquivo
-    it('Login com sucesso', () => {
+      // Sixth Part
+      selectBloodType: '.orangehrm-card-container > .oxd-form > .oxd-form-row > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input',
+      inputTestField: '.orangehrm-card-container > .oxd-form > .oxd-form-row > .oxd-grid-3 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input',
+    
+      // Save Elements
+      buttonSave: "[type='submit']",
+      toastTitleSuccess: ".oxd-text--toast-title",
+      toastMessageSuccess: ".oxd-text--toast-message"
+    };
+    
+    // Successful login test with the Const and the object userData in another file
+    it('Successful login', () => {
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-      cy.get(elemento.inserirNome) .type(userData.usuarioCorreto.nome) /// USUARIO CORRETO   
-      cy.get(elemento.inserirSenha) .type(userData.usuarioCorreto.senha) /// SENHA CORRETA
-      cy.get(elemento.clicarEmSubmit).click() /// CLICAR EM LOGIN
-      cy.location('pathname').should('equal','/web/index.php/dashboard/index')///SISTEMA RETORNA PAGINA DO DASHBOARD
-      cy.get(elemento.paginaDashboard)/// SISTEMA RETORNA TITULO DA PAGINA = DASHBOARD
-    })
-
-    // Login senha errada porem só com os elemento sem o userData
-    it('login senha errada',() => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(elemento.inserirNome) .type('Admin') /// USUARIO CORRETO
-    cy.get(elemento.inserirSenha) .type('test') /// SENHA ERRADA 
-    cy.get(elemento.clicarEmSubmit).click() /// CLICAR EM LOGIN
-    cy.get(elemento.alerta).contains('Invalid credentials') /// RETORNAR MENSAGEM DE ERRO 'INVALID CREDENTIALS'
-  
+      cy.get(pageElements.inputUsername).type(userData.correctUser.user) // Correct username
+      cy.get(pageElements.inputPassword).type(userData.correctUser.password) // Correct password
+      cy.get(pageElements.buttonSubmit).click() // Click login
+      cy.location('pathname').should('equal', '/web/index.php/dashboard/index') // System returns dashboard page
+      cy.get(pageElements.dashboardPage) // System returns dashboard page title
+    });
     
-
-    // login com usuário errado porem só com os elemento sem o userData(somente no nome)
-
-
-    })
-  
-    // Login com usuário errado porem só com os elemento sem o userData(somente no nome)
-    it('Login usuário errado', () => {
+    // Login with incorrect password but only using elements without userData
+    it('Login with incorrect password', () => {
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-      cy.get(elemento.inserirNome) .type(userData.usuarioErrado.nome) /// USUARIO ERRADO  
-      cy.get(elemento.inserirSenha) .type('admin122') /// SENHA CORRETA
-      cy.get(elemento.clicarEmSubmit).click() /// CLICAR EM LOGIN
-      cy.get(elemento.alerta).contains('Invalid credentials') /// RETORNAR MENSAGEM DE ERRO 'INVALID CREDENTIALS'
-      
-    })
+      cy.get(pageElements.inputUsername).type('Admin') // Correct username
+      cy.get(pageElements.inputPassword).type('test') // Incorrect password
+      cy.get(pageElements.buttonSubmit).click() // Click login
+      cy.get(pageElements.alert).contains('Invalid credentials') // Return error message 'Invalid credentials'
+    });
     
+    // Login with incorrect username but only using elements without userData (only the name)
+    it('Login with incorrect username', () => {
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+      cy.get(pageElements.inputUsername).type(userData.incorrectUser.name) // Incorrect username
+      cy.get(pageElements.inputPassword).type('admin122') // Correct password
+      cy.get(pageElements.buttonSubmit).click() // Click login
+      cy.get(pageElements.alert).contains('Invalid credentials') // Return error message 'Invalid credentials'
+    });
     
-    //Entendendo como funciona o baseUrl
-    //baseUrl: https://opensource-demo.orangehrmlive.com/web/index.php
-    it('Login usuário errado 2 ', () => {
+    // Understanding how baseUrl works
+    // baseUrl: https://opensource-demo.orangehrmlive.com/web/index.php
+    it('Login with incorrect username 2', () => {
       cy.visit('/auth/login')
-      cy.get(elemento.inserirNome) .type(userData.usuarioErrado.nome) /// USUARIO ERRADO  
-      cy.get(elemento.inserirSenha) .type('admin122') /// SENHA CORRETA
-      cy.get(elemento.clicarEmSubmit).click() /// CLICAR EM LOGIN
-      cy.get(elemento.alerta).contains('Invalid credentials') /// RETORNAR MENSAGEM DE ERRO 'INVALID CREDENTIALS'
-      
-    })
-
-
-    //Novo cenário para acessar o myinfo e preencher as informações do usuário
-    it.only('Login,acesso ao myinfo e inserir dados', () => {
-      
-      //Logando no sistema
+      cy.get(pageElements.inputUsername).type(userData.incorrectUser.name) // Incorrect username
+      cy.get(pageElements.inputPassword).type('admin122') // Correct password
+      cy.get(pageElements.buttonSubmit).click() // Click login
+      cy.get(pageElements.alert).contains('Invalid credentials') // Return error message 'Invalid credentials'
+    });
+    
+    // New scenario to access My Info and fill in user information
+    it('Login, access My Info and fill in details', () => {
+      // Logging into the system
       cy.visit('/auth/login')
-      cy.get(elemento.inserirNome) .type(userData.usuarioCorreto.nome) /// USUARIO CORRETO
-      cy.get(elemento.inserirSenha) .type(userData.usuarioCorreto.senha) /// SENHA CORRETA
-      cy.get(elemento.clicarEmSubmit).click() /// CLICAR EM LOGIN
-      cy.location('pathname').should('equal','/web/index.php/dashboard/index')///SISTEMA RETORNA PAGINA DO DASHBOARD
-      cy.get(elemento.paginaDashboard)/// SISTEMA RETORNA TITULO DA PAGINA = DASHBOARD  
-      
-      //Acessando o myinfo
-      cy.get(elemento.myInfobotao).contains('My Info').click() /// CLICAR EM MYINFO
-      cy.location('pathname').should('equal','/web/index.php/pim/viewPersonalDetails/empNumber/7')///SISTEMA RETORNA PAGINA DO MYINFOS 
-
-      //Preenchendo informações do usuário(Primeiro nome, sobrenome, ultimo nome)
-      cy.get(elemento.primeiroNome).click().clear().type('Teste')/// PREENCHER NOME
-      cy.get(elemento.sobrenome).click().clear().type('Teste') /// PREENCHER SOBRENOME
-      cy.get(elemento.ultimoNome).click().clear().type('Teste') /// PREENCHER SOBRENOME
-
-     //Preenchendo informações do usuário segunda parte (ID, OTHER ID, DRIVE LICENSE, EXPIRY DATE)
-      cy.get(elemento.employeeId).click().clear().type('123456') /// PREENCHER ID
-      cy.get(elemento.otherId).click().clear().type('654321') /// PREENCHER OTHER ID
-      cy.get(elemento.driveLicense).click().clear().type('123456789') /// PREENCHER DRIVE LICENSE
-      cy.get(elemento.inputCalenderlicenseExpiryDate).click().clear().type('2030-25-12') /// PREENCHER DATA DE EXPIRAÇÃO
-      cy.get(elemento.inputCalenderlicenseExpiryDate).click() /// CLICAR NO INPUT DE DATA PARA FECHAR O CALENDARIO
-      
-      //Preenchendo informações do usuário terceira parte (nacionalidade, estado civil, data de nascimento e gênero)
-     cy.get(elemento.nacionalityStatus).eq(0).click().type('{downarrow}'.repeat(26))
-     cy.get(elemento.nacionalityStatus).eq(0).contains('Brazilian')
-     cy.get(elemento.nacionalityStatus).eq(1).click().type('{downarrow}'.repeat(1)).click()
-     cy.get(elemento.nacionalityStatus).eq(1).contains('Single')
-      
-
-     //quinta parte
-     cy.get(elemento.dataNascimento).click().clear().type('1990-25-12'), /// PREENCHER DATA DE NASCIMENTO
-      cy.get(elemento.dataNascimento).click() /// CLICAR NO INPUT DE DATA PARA FECHAR O CALENDARIO
-      cy.get(elemento.femaleButton).click() /// SELECIONAR GENERO FEMININO
-
-
+      cy.get(pageElements.inputUsername).type(userData.correctUser.user) // Correct username
+      cy.get(pageElements.inputPassword).type(userData.correctUser.password) // Correct password
+      cy.get(pageElements.buttonSubmit).click() // Click login
+      cy.location('pathname').should('equal', '/web/index.php/dashboard/index') // System returns dashboard page
+      cy.get(pageElements.dashboardPage) // System returns dashboard page title
     
-      //sexta parte 
-      cy.get(elemento.bloodType).click().type('{downarrow}'.repeat(4)).click() /// SELECIONAR BLOOD TYPE b+
-      cy.get(elemento.testField).click().clear().type('TESTANDO!!!!!!') /// PREENCHER TEST FIELD
-
-
-
-      // Salva as informações
-     cy.get(elemento.saveButton).eq(0).click() /// CLICAR EM SAVE
-     cy.get(elemento.toastTitleSuccess).contains('Success') /// RETORNAR MENSAGEM DE SUCESSO
-     cy.get(elemento.toastMensageSuccess).contains('Successfully Updated') /// RETORNAR MENSAGEM DE SUCESSO
+      // Accessing My Info
+      cy.get(pageElements.buttonMyInfo).contains('My Info').click() // Click My Info
+      cy.location('pathname').should('equal', '/web/index.php/pim/viewPersonalDetails/empNumber/7') // System returns My Info page
     
-
-
-      //cy.get(elemento.dropdownNacionalidade).select('Brazilian') /// SELECIONAR NACIONALIDADE
-
-
-      
-      
+      // Filling in user information (first name, middle name, last name)
+      cy.get(pageElements.inputFirstName).click().clear().type('Test') // Fill in first name
+      cy.get(pageElements.inputMiddleName).click().clear().type('Test') // Fill in middle name
+      cy.get(pageElements.inputLastName).click().clear().type('Test') // Fill in last name
+    
+      // Filling in user information second part (ID, OTHER ID, DRIVER LICENSE, EXPIRY DATE)
+      cy.get(pageElements.inputEmployeeId).click().clear().type('123456') // Fill in employee ID
+      cy.get(pageElements.inputOtherId).click().clear().type('654321') // Fill in other ID
+      cy.get(pageElements.inputDriverLicense).click().clear().type('123456789') // Fill in driver license
+      cy.get(pageElements.inputLicenseExpiryDate).click().clear().type('2030-25-12') // Fill in expiry date
+      cy.get(pageElements.inputLicenseExpiryDate).click() // Click the date input to close the calendar
+    
+      // Filling in user information third part (nationality, marital status, date of birth, and gender)
+      cy.get(pageElements.nationalityStatus).eq(0).click().type('{downarrow}'.repeat(26))
+      cy.get(pageElements.nationalityStatus).eq(0).contains('Brazilian')
+      cy.get(pageElements.nationalityStatus).eq(1).click().type('{downarrow}'.repeat(1)).click()
+      cy.get(pageElements.nationalityStatus).eq(1).contains('Single')
+    
+      // Fifth part
+      cy.get(pageElements.inputDateOfBirth).click().clear().type('1990-25-12') // Fill in date of birth
+      cy.get(pageElements.inputDateOfBirth).click() // Click the date input to close the calendar
+      cy.get(pageElements.femaleButton).click() // Select female gender
+    
+      // Sixth part
+      cy.get(pageElements.selectBloodType).click().type('{downarrow}'.repeat(4)).click() // Select blood type B+
+      cy.get(pageElements.inputTestField).click().clear().type('TESTING!!!!!!') // Fill in test field
+    
+      // Save the information
+      cy.get(pageElements.buttonSave).eq(0).click() // Click save
+      cy.get(pageElements.toastTitleSuccess).contains('Success') // Return success message
+      cy.get(pageElements.toastMessageSuccess).contains('Successfully Updated') // Return success message
+    });
       
 
       
@@ -167,7 +141,7 @@ describe('site Orange', () => {
 
   
   
-  })
+  
 
 
   
